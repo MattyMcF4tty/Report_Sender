bfe03a309e3433d83cbf23607692bf5e681fc976
MattyMcF4tty <matthias.r.kristensen@gmail.com>
2023-03-18 15:39:55 +0100

    Initialization and setup of project

---
+++ reportsender/lib/screens/damageReport.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class DamageReport extends StatelessWidget {
+    const DamageReport({super.key});
+
+    @override
+    Widget build(BuildContext context) {
+      return MaterialApp(
+        title: 'Damage Report',
+        theme: ThemeData(
+
+        ),
+      );
+    }
+
+}
